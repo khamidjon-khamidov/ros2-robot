30b1ec4c6ef7abaa2e18770a6c205281aa1ce5e6
khamidjon-khamidov <hamidovhamid1998@gmail.com>
2025-09-12 10:29:23 +0300

    update setup

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'IAS0220_246075IVSM'
+package_name = "IAS0220_246075IVSM"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "urdf"), glob("urdf/*.urdf")),
+        (os.path.join("share", package_name, "config"), glob("config/*.rviz")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='hamidjon',
-    maintainer_email='hamidovhamid1998@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="hamidjon",
+    maintainer_email="hamidovhamid1998@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
